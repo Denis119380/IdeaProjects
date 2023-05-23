3e668e246a879ba3eb31b763567672563442fee0
Denis Ryzhakov <denis.r@mail.ru>
2023-05-23 19:16:29 +0500

    First commit java files of InterfaceExtends_Shildt_9 projects

---
+++ JAVA_SHILDT/InterfaceExtends_Shildt_9/src/B.java
@@ -0,0 +1,6 @@
+// Теперь интерфейс B включает в себя методы meth1()
+// и meth2() и добавляет метод meth3()
+interface B extends A{
+
+    void meth3();
+}
---
+++ JAVA_SHILDT/InterfaceExtends_Shildt_9/src/MyClass.java
@@ -0,0 +1,16 @@
+// Этот класс должен реализовать все методы
+// из интерфейсов A и B
+class MyClass implements B{
+
+    public void meth1() {
+        System.out.println("Реализация метода meth1().");
+    }
+
+    public void meth2() {
+        System.out.println("Реализация метода meth2().");
+    }
+
+    public void meth3() {
+        System.out.println("Реализация метода meth3().");
+    }
+}
