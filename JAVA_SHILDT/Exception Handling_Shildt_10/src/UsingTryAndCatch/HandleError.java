813f36bdfd8cea45bfe0c40d11345fd48ea35c90
Denis Ryzhakov <denis.r@mail.ru>
2023-05-31 08:14:37 +0500

    Add commit HandleError.java to the Excetion Handling project.

---
+++ JAVA_SHILDT/Exception Handling_Shildt_10/src/UsingTryAndCatch/HandleError.java
@@ -0,0 +1,28 @@
+package UsingTryAndCatch;
+
+import java.util.Random;
+
+public class HandleError {
+
+    public static void main(String[] args) {
+        // обработать исключение и продолжить работу
+
+        int a = 0, b = 0, c = 0;
+        Random r = new Random(); // создание экземпляра класса Random
+                                 // генерирует поток псевдослучайных чисел
+
+        for (int i = 0; i < 50; i++) {
+            try { // отслеживание ошибок
+                b = r.nextInt(); // метод возвращает псевдослучайное
+                c = r.nextInt(); // целочисленное значение (может отрицательное)
+                a = 12345 / (b / c);
+            } catch (ArithmeticException e) { // перехват исключения (ошибки)
+                                              // с указанием типа исключения
+                System.out.println("Деление на нуль.");
+                a = 0; // присвоить нуль и продолжить работу
+            }
+            System.out.println("a: " + a);
+        }
+
+    }
+}
